8169130f8a26af235aa886815a9bf1a80c5a8251
soulaimane yahya <soulaimaneyh07@gmail.com>
2022-11-12 11:05:59 +0100

    fixing-some-issues

---
+++ database/migrations/2022_11_12_095627_add_slug.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('products', function(Blueprint $table) {
+            $table->string('slug')->unique()->after('name');
+        });
+        Schema::table('categories', function(Blueprint $table) {
+            $table->string('slug')->unique()->after('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('products', function(Blueprint $table) {
+            $table->dropColumn('slug');
+        });
+        Schema::table('categories', function(Blueprint $table) {
+            $table->dropColumn('slug');
+        });
+    }
+};
